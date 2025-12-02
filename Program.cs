f9b18b96af6c0d042a48780940fd5605a91a1c24
Noa B <noaboserup@gmail.com>
2025-12-02 10:19:18 +0100

    Added show_correct_guesses(), guess() and guess_loop()

--- Program.cs
+++ Program.cs
@@ -76,21 +76,34 @@ namespace hangman
 "
             };
             Word word = new Word();
+            Wrong_guesses wrong = new Wrong_guesses();
             word.show_word();
+            word.show_correct_guesses();
             Console.Write(gallows[0]);
+
+            word.guess();
+
+            word.show_correct_guesses();
         }
+
     }
+    
     class Word
     {
         char[] word;
         string[] wordlist = {"apple", "river", "mountain", "dream", "light", "forest", "shadow", "ocean", "storm", "music", "flame", "whisper", "mirror", "stone", "gold", "silver", "cloud", "garden", "path", "wind", "star", "flower", "sand", "sky", "rain", "tree", "valley", "island", "echo", "fire", "wave", "heart", "night", "sun", "moon", "bird", "leaf", "glass", "snow", "rose", "bridge", "tower", "field", "door", "voice", "dreamer", "seed", "song", "time", "hope", "biatch"};
-        char[] correct_guesses;
-        int letter_pos;
+        char[] correct;
 
         public Word()
         {
             string temp_word = randomize();
             word = temp_word.ToCharArray(0,temp_word.Length);
+
+            correct = new char[word.Length];
+            for (int i = 0; i < word.Length; i++)
+            {
+                correct[i] = '_';
+            }
         }
 
         public string randomize()
@@ -102,6 +115,37 @@ namespace hangman
         public void show_word()
         {
             Console.WriteLine(word);
+        }
+
+        public void show_correct_guesses()
+        {
+            for (int i = 0; i < correct.Length; i++)
+            {
+                Console.Write(correct[i]);
+                Console.Write(" ");
+            }
+        }
+
+        public void guess()
+        {
+            Console.WriteLine("Guess: ");
+            char guess = Char.ToLower(Convert.ToChar(Console.ReadLine()));
+
+            if (word.Contains(guess))
+            {
+                guess_loop(guess);
+            }
+        }
+
+        private void guess_loop(char guess)
+        {
+            for (int i = 0; 0 < word.Length; i++)
+            {
+                if (word[i] == guess)
+                {
+                    word[i] = guess;
+                }
+            }
         }
     }
 
